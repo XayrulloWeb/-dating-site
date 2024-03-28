623a232fba815ff59f6a1031ddaa1ee1a6304b72
S7L5An <152207628+S7L5An@users.noreply.github.com>
2024-03-28 12:48:04 +0500

    add register

--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,30 @@
 import Login from "./Login/Login";
+import Register from "./Register/Register"
 import Home from "./Companents/Home/Home";
 import User from "./Companents/User/User";
-
 
 const routes = [
     {
 
-        id:1,
+        id: 1,
         path: '/',
         component: Home,
     },
     {
-        id:2,
+        id: 2,
         path: "/login",
         component: Login,
     },
     {
-        id:3,
+        id: 3,
         path: "/user",
         component: User,
     },
-
+    {
+        id: 4,
+        path: "/register",
+        component: Register,
+    }
 
 ]
 export default routes
